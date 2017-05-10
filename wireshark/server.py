ae7b4c477d7d07778f4b149e9ed4a68d017a6a05
Irina Gossmann <igossmann@palantir.com>
2017-05-10 12:45:25 -0400

    Add sample http pcap and example server

---
+++ wireshark/server.py
@@ -0,0 +1,17 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+#import ssl
+
+# This handler says MEOW to all GET requests
+class MeowHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write("MEOW!".encode("utf-8"))
+
+server_address = ('localhost', 4443)
+httpd = HTTPServer(server_address, MeowHandler)
+#httpd.socket = ssl.wrap_socket(httpd.socket, keyfile='server.pem', certfile='server.crt', server_side=True)
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    httpd.server_close()
